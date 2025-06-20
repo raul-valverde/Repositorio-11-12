4754e3c707509ccd2367ceb62976d95a0cf7376d
raul-valverde <raulvruiz@uamv.edu.ni>
2025-06-20 10:04:51 -0600

    Update Ejercicio1.py

--- Martes17_06CRUD/Ejercicio1.py
+++ Martes17_06CRUD/Ejercicio1.py
@@ -2,9 +2,12 @@
 def leer_poema():
     try:
         with open("poema.txt", "r", encoding="utf-8") as archivo:
+            #Le asignamos el arhivo que va a leer
             for linea in archivo:
                 print(linea.strip())
+                #que lo imprima linea por linea
     except FileNotFoundError:
+        #si el archivo no existe o no lo encuentra, se enviara un print con un mensaje de error
         print("El archivo 'poema.txt' no existe.")
 
 leer_poema()
