3560dd6a378386e3d4ab8160099c4c03942f2015
raul-valverde <raulvruiz@uamv.edu.ni>
2025-06-20 10:11:36 -0600

    Update ejercicio7.py

--- Martes17_06CRUD/ejercicio7.py
+++ Martes17_06CRUD/ejercicio7.py
@@ -2,19 +2,19 @@
 # nombre,calificacion1,calificacion2,calificacion3
 def calcular_promedios():
     try:
-        with open("estudiantes.csv", "r", encoding="utf-8") as archivo_entrada, \
-             open("promedios.txt", "w", encoding="utf-8") as archivo_salida:
+        with open("estudiantes.csv", "r", encoding="utf-8") as archivo_entrada, \ #Le asignamos la apertura del archivo que va a leer
+             open("promedios.txt", "w", encoding="utf-8") as archivo_salida: #le indicamos que abra el archivo en el cual va a escribir
             for linea in archivo_entrada:
                 partes = linea.strip().split(",")
-                nombre = partes[0]
+                nombre = partes[0] #dividimos partes
                 try:
                     calificaciones = list(map(float, partes[1:]))
                     if calificaciones:
-                        promedio = sum(calificaciones) / len(calificaciones)
+                        promedio = sum(calificaciones) / len(calificaciones) #se calcula el promedio
                         archivo_salida.write(f"{nombre},{promedio:.2f}\n")
                 except ValueError:
-                    archivo_salida.write(f"{nombre},Error en calificaciones\n")
+                    archivo_salida.write(f"{nombre},Error en calificaciones\n")#S evalua error en escritura de las calificaicones
     except FileNotFoundError:
-        print("El archivo 'notas.csv' no existe.")
+        print("El archivo 'notas.csv' no existe.")#Se evalua error en el archivo de notas.csv
 
-calcular_promedios()
+calcular_promedios()#Se corre el programa
