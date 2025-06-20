31c53089430d0875f31c886547285d11ef0d597e
raul-valverde <raulvruiz@uamv.edu.ni>
2025-06-20 11:00:03 -0600

    Delete archivo11 directory

--- archivo11/ej6.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def area_circulo(radio):
-    """Devuelve el área de un círculo dado su radio."""
-    return math.pi * radio ** 2
-
-radio = float(input("Ingrese el radio del círculo: "))
-print(f"El área del círculo es: {area_circulo(radio):.2f}")
